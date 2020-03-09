fc230396dd8681426eb30e60292641b1bf85c614
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-09 00:22:48 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
-def unsafe?(speed)
+#if speed 40> or 60< its unsafe. 
 
+def unsafe?(speed)
+  
+
+end 
 end
 
-
+#if speed 40> or 60< its safe. 
 
 def not_safe?(speed)
-	
+  if speed is 40> || <60 = safe
+end
+if speed == speed <40|| speed >60 ? true:false
+end 
 end
 	
 
